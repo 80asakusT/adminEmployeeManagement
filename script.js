5b23005884a310cd74a344e4a71adb2b1fc756b3
AnhHT3010 <trunganhhoang2001@gmail.com>
2025-04-05 18:39:19 +0700

    form add employee

--- script.js
+++ script.js
@@ -42,31 +42,43 @@ renderEmployees(employees);
 
 // Làm sao để lấy được giá trị của input Name => value
 
-
-
 // Khi click nút Apply Filters => addEventListener("click", () => {})
 
 const button = document.getElementById("applyFilters");
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   renderEmployees(filterEmployees(employees));
 });
 
-
 // cần hàm filterEmployees(cần 1 danh sách nhân viên theo điều kiện đã lọc)
 
-function filterEmployees(arr){
-  var employeeCriteria = {name : "", department : "", age : "", salary : ""};
+function filterEmployees(arr) {
+  var employeeCriteria = { name: "", department: "", age: "", salary: "" };
   const temp = employeeCriteria;
   employeeCriteria.name = document.getElementById("filterName").value;
-  employeeCriteria.department = departments.options[departments.selectedIndex].text;
+  employeeCriteria.department =
+    departments.options[departments.selectedIndex].text;
   employeeCriteria.age = document.getElementById("filterAge").value;
   employeeCriteria.salary = document.getElementById("filterSalary").value;
   let filtered = arr.filter(
     (employee) =>
-      (employee.name.includes(employeeCriteria.name)) &&
-      (employee.department == employeeCriteria.department || employeeCriteria.department == "All") &&
+      employee.name.includes(employeeCriteria.name) &&
+      (employee.department == employeeCriteria.department ||
+        employeeCriteria.department == "All") &&
       (employee.age == employeeCriteria.age || employeeCriteria.age == "") &&
-      (employee.salary == employeeCriteria.salary || employeeCriteria.salary == "")
+      (employee.salary == employeeCriteria.salary ||
+        employeeCriteria.salary == "")
   );
   return filtered;
 }
+
+//  Form add Employee
+// B1: Lấy các value của input trong form
+const formAddEmployee = document.querySelector("#addEmployeeForm");
+// B2: Validate các value: (các trường khôgn được để trống, tuổi nằm trong khoảng > 0, < 60 or < 70, department trong danh sách department không All)
+formAddEmployee.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("submit");
+});
+// B3: Show lỗi ở dưới value không hợp lệ
+// B4: Nếu không có lỗi => thêm nhân viên vào danh sách employees
+// B5: Render lại danh sách nhân viên
