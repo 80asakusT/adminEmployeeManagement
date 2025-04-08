d04c4b6fd23ff57d8f1eb8cb074f92631d79f736
80asakusT <dangleminh08@gmail.com>
2025-04-08 19:15:02 +0700

    addEmp

--- script.js
+++ script.js
@@ -71,14 +71,52 @@ function filterEmployees(arr) {
   return filtered;
 }
 
-//  Form add Employee
+// Form add Employee
 // B1: Lấy các value của input trong form
 const formAddEmployee = document.querySelector("#addEmployeeForm");
-// B2: Validate các value: (các trường khôgn được để trống, tuổi nằm trong khoảng > 0, < 60 or < 70, department trong danh sách department không All)
+
+let errName = document.querySelector("#errName");
+let errAge = document.querySelector("#errAge");
+let errDepartment = document.querySelector("#errDepartment");
+let errSalary = document.querySelector("#errSalary");
+
+function validate(name, age, department, salary) {
+  if (name == "") errName.innerHTML = "Vui lòng điền tên";
+  else errName.innerHTML = "";
+  if (age < 18 || age > 60 || age == "")
+    errAge.innerHTML = "Tuổi không hợp lệ";
+  else errAge.innerHTML = "";
+  if (department == "") errDepartment.innerHTML = "Vui lòng chọn phòng ban";
+  else errDepartment.innerHTML = "";
+  if (salary < 0 || salary == "")
+    errSalary.innerHTML = "Lương không hợp lệ";
+  else errSalary.innerHTML = "";
+  return !(
+    errName.innerText ||
+    errAge.innerText ||
+    errDepartment.innerText ||
+    errSalary.innerText
+  );
+}
+
+// B2: Validate các value: (các trường không được để trống, tuổi nằm trong khoảng > 0, < 60 or < 70, department trong danh sách department không All)
 formAddEmployee.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("submit");
+  let nameAdd = document.getElementById("nameAdd");
+  let ageAdd = document.getElementById("ageAdd");
+  let departmentAdd = document.getElementById("departmentAdd");
+  let salaryAdd = document.getElementById("salaryAdd");
+  console.log(nameAdd, ageAdd, departmentAdd, salaryAdd);
+  if (!validate(nameAdd.value, ageAdd.value, departmentAdd.value, salaryAdd.value)) return;
+  employees.push({ id: employees.length+1, name: nameAdd.value, age: ageAdd.value, salary: salaryAdd.value, department: departmentAdd.value});
+  formAddEmployee.reset();
+  renderEmployees(employees);
 });
 // B3: Show lỗi ở dưới value không hợp lệ
 // B4: Nếu không có lỗi => thêm nhân viên vào danh sách employees
 // B5: Render lại danh sách nhân viên
+
+// nameAdd.addEventListener("input", (e) => {
+//   if (nameAdd == "") errName.innerHTML = "Vui lòng điền tên";
+//   else errName.innerHTML = "";
+// });
